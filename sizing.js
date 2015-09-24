68aec8de3e982d747d97b12b8b67b378c053db63
Matthew <matthew.1142@hotmail.com>
2015-09-24 19:34:20 -0400

    Got drop-down menu working properly

--- sizing.js
+++ sizing.js
@@ -1,13 +1,17 @@
 
 var toggle = document.getElementsByClassName("toggle")[0];
 var menu = document.getElementsByClassName("menu")[0];
+var content = document.getElementsByClassName("content")[0];
 
 function toggleMenu() {
 	if (menu.style.display != "inline") {
+		content.style.zIndex = "-1";
 		menu.style.display = "inline";
+		
 	}
 	else {
 		menu.style.display = "none";
+		content.style.zIndex = "1";
 	}
 }
 
