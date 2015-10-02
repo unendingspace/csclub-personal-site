1f925fb240f15f00d425a4553fa31d9e4d4571e7
Matthew <matthew.1142@hotmail.com>
2015-10-02 19:56:56 -0400

    Changed content font

--- contentfetch.js
+++ contentfetch.js
@@ -8,8 +8,10 @@ var active = null;
 function toggleContentTab() {
 	if (content_window.style.display != "inline") {
 		content_window.style.display = "inline";
+		content_window.style.opacity = "0.6";
 	}
 	else {
+		content_window.style.opacity = "0";
 		content_window.style.display = "none";
 	}
 }
